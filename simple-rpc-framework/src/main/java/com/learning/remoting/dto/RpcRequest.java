324e6dcbbf800f6f7f5bd6ada73c0bcd73842f88
Lykr <674634684@qq.com>
2021-03-01 13:56:03 +0800

    Complete dto

--- simple-rpc-framework/src/main/java/com/learning/remoting/dto/RpcRequest.java
+++ simple-rpc-framework/src/main/java/com/learning/remoting/dto/RpcRequest.java
@@ -1,4 +1,27 @@
 package com.learning.remoting.dto;
 
-public class RpcRequest {
+import com.learning.properties.ServiceProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RpcRequest implements Serializable {
+    private static final long serialVersionUID = 6180303740933980432L;
+    private String requestId;
+    private String interfaceName;
+    private String methodName;
+    private Object[] parameters;
+    private Class<?>[] paramTypes;
+    private String version;
+
+    public ServiceProperties getServiceProperties() {
+        return new ServiceProperties(interfaceName, version);
+    }
 }
--- simple-rpc-framework/src/main/java/com/learning/remoting/dto/RpcResponse.java
+++ simple-rpc-framework/src/main/java/com/learning/remoting/dto/RpcResponse.java
@@ -1,4 +1,30 @@
 package com.learning.remoting.dto;
 
-public class RpcResponse {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RpcResponse implements Serializable {
+    private static final long serialVersionUID = 6590520280737965182L;
+    /**
+     * 200 - success
+     * 400 - failure
+     */
+    private Integer code;
+    private Object data;
+
+    public static RpcResponse success(Object data) {
+        return new RpcResponse(200, data);
+    }
+
+    public static RpcResponse failure() {
+        return new RpcResponse(400, null);
+    }
 }
